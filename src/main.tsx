b58caf5eebdcfa1e18cd5d61d1332966430209be
Anilabh Barua <anilabh.barua@gmail.com>
2025-05-01 01:38:01 +0530

    updated main.tsx for LocalStorage

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { LocalStorageProvider } from './contexts/LocalStorageContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LocalStorageProvider>
+      <App />
+    </LocalStorageProvider>
   </StrictMode>
 );
